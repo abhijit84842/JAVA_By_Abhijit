4f2da872e8d06f4dfe9959331c81a098c71a47d7
Abhijit Das <abhijitd721201@gmail.com>
2023-12-18 18:06:39 +0530

    first commit

---
+++ JAVA Interface/Intro interface/interfacecls.java
@@ -0,0 +1,19 @@
+interface CustomerRaju{
+    int rice=5; // public + static + final
+    void purches(); // public + abstract
+}
+class ShellerAbhijit implements CustomerRaju{
+    @Override
+    public void purches(){
+            //rice=7;   // we can't change the interface value, so it is final
+            System.out.println("Raju need" + rice +"kg rice");
+    }
+}
+public class interfacecls{
+    public static void main(String[] args) {
+        ShellerAbhijit obj=new ShellerAbhijit();
+        obj.purches();
+
+        System.out.println(CustomerRaju.rice);
+    }
+}
